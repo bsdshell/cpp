4efe0a7a82be21be390360addf1563f327062ed0
bsdshell <000b000@gmail.com>
2014-07-03 23:47:42 -0700

    print spiral in c++

--- PrintSpiral.cpp
+++ PrintSpiral.cpp
@@ -2,14 +2,22 @@
 
 int main()
 {
+    /*
     int row = 4;
-    int col = 4;
-    int Array[4][4] = {
-        { 1,  2,  3, 4},
-        {12, 13, 14, 5},
-        {11, 16, 15, 6},
-        {10,  9,  8, 7}
+    int col = 6;
+    int Array[4][6] = {
+        { 1,   2,  3,  4,  5, 6},
+        {16,  17, 18, 19, 20, 7},
+        {15,  24, 23, 22, 21, 8},
+        {14,  13, 12, 11, 10, 9}
     };
+    */
+    int row = 1;
+    int col = 6;
+    int Array[1][6] = {
+        { 1,   2,  3,  4,  5, 6}
+    };
+
 
     for(int i=0; i<row; i++)
     {
@@ -19,31 +27,35 @@ int main()
         }
         std::cout<<std::endl;
     }
-
     std::cout<<std::endl;
-    for(int i=0; i<row/2; i++)
-    {
-        for(int j=i; j<col-1-i; j++)
-        {
-            std::cout<<"["<<Array[i][j]<<"]";
-        }
-        std::cout<<std::endl;
-        for(int j=i; j<col-1-i; j++)
-        {
-            std::cout<<"["<<Array[j][col-1-i]<<"]";
-        }
-        std::cout<<std::endl;
-        for(int j=i; j<col-1-i; j++)
-        {
-            std::cout<<"["<<Array[row-1-i][col-1-j]<<"]";
-        }
-        std::cout<<std::endl;
-        for(int j=i; j<col-1-i; j++)
-        {
-            std::cout<<"["<<Array[row-1-j][i]<<"]";
-        }
-        std::cout<<std::endl;
-    }
 
+    if(col > row)
+    {
+        for(int i=0; i<row/2; i++)
+        {
+            for(int j=i; j<col-1-i; j++)
+            {
+                std::cout<<"["<<Array[i][j]<<"]";
+            }
+            std::cout<<std::endl;
+            for(int j=i; j<row-1-i; j++)
+            {
+                std::cout<<"["<<Array[j][col-1-i]<<"]";
+            }
+            std::cout<<std::endl;
+
+            for(int j=i; j<col-1-i; j++)
+            {
+                std::cout<<"["<<Array[row-1-i][col-1-j]<<"]";
+            }
+            std::cout<<std::endl;
+
+            for(int j=i; j<row-1-i; j++)
+            {
+                std::cout<<"["<<Array[row-1-j][i]<<"]";
+            }
+            std::cout<<std::endl;
+        }
+    }
     std::cout<<"spiral"<<std::endl;
 }
