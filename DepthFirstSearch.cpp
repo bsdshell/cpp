b5d55fe0b96fea2dc86aa4cad37e63a13d7a0733
bsdshell <000b000@gmail.com>
2014-08-09 02:19:56 -0700

    Search a path in Maze with Depth First Search and print out the path if a path is found

--- DepthFirstSearch.cpp
+++ DepthFirstSearch.cpp
@@ -2,17 +2,31 @@
 #include<stack>
 using namespace std;
 
+void PrintLocation(int down, int right)
+{
+    string str = std::to_string(down) + " " + std::to_string(right);
+    std::cout<<"["<<str<<"]"<<std::endl;
+}
+
+void PrintMaze(int array[][6], int width, int height)
+{
+    for(int i=0; i<5; i++)
+    {
+        for(int j=0; j<6; j++)
+        {
+            if(down == i && j == right)
+                std::cout<<"["<<"x"<<"]";
+            else
+                std::cout<<"["<<array[i][j]<<"]";
+        }
+        std::cout<<std::endl;
+    }
+}
 bool DepthFirstSearch(int array[][6], int down, int right)
 {
     if(down < 5 && right < 6) 
     {
-        if(DepthFirstSearch(array, down+1, right)) 
-        {
-            string str = std::to_string(down) + " " + std::to_string(right);
-            std::cout<<"["<<str<<"]"<<std::endl;
-            return true;
-        }
-        else if(array[down][right] == 0)
+        if(array[down][right] == 0 || array[down][right] == 2)
         {
             for(int i=0; i<5; i++)
             {
@@ -27,22 +41,29 @@ bool DepthFirstSearch(int array[][6], int down, int right)
             }
             std::cout<<std::endl;
 
-            if(!DepthFirstSearch(array, down+1, right)) 
-                return false;
-            if(!DepthFirstSearch(array, down, right+1))     
-                return false;
-            return false;
-        }
-        else if(array[down][right] == 2)
-        {
-            std::cout<<"Done"<<std::endl;
-            string str = std::to_string(down) + " " + std::to_string(right);
-            std::cout<<str<<std::endl;
-            return true;
+            if(DepthFirstSearch(array, down+1, right)) 
+            {
+                PrintLocation(down, right); 
+                return true;
+            }
+
+            if(array[down][right] == 2)
+            {
+                std::cout<<"Done"<<std::endl;
+                PrintLocation(down, right); 
+                return true;
+            }
+
+            if(DepthFirstSearch(array, down, right+1))     
+            {
+                PrintLocation(down, right); 
+                return true;
+            }
         }
     }
     return false;
 }
+
 
 int main(int argc, char *argv[])
 {
