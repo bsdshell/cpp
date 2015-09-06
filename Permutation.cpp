2702262ba33c37e4e4d7450f77cf8aa33786f301
bsdshell <monosurface@gmail.com>
2015-09-06 14:44:54 -0700

    add combination based on Binomial Identity C(n, k) = C(n-1, k-1) + C(n-1, k)

--- Permutation.cpp
+++ Permutation.cpp
@@ -34,6 +34,10 @@ class MyClass
     {
         for(int i=k; i<size; i++)
         {
+            for(int j=0; j<k; j++)
+                std::cout<<"[--]";
+            std::cout<<"(i="<<i<<",k="<<k<<")"<<std::endl;
+
             buf[strlen(buf)] = array[i];
             buf[strlen(buf)+1] = '\0';
             for(int j=0; j<strlen(buf); j++)
@@ -45,26 +49,96 @@ class MyClass
             buf[strlen(buf)-1] = '\0'; 
         }
     }
+    void nchoosek(char array[], char buf[], int n, int r, int k)
+    {
+        if( k > n)
+            return;
+        if(k == 0 || n == 0)
+        {
+            for(int i=1; i<=r; i++)
+            {
+                std::cout<<"["<<buf[i]<<"]";
+            }
+            std::cout<<std::endl;
+        }
+        else{
+            buf[k] = array[n];
+            nchoosek(array, buf, n-1, r, k-1);
+            nchoosek(array, buf, n-1, r, k);
+        }
+    }
     void swap(int* array, int i, int j)
     {
         int tmp = array[i];
         array[i] = array[j];
         array[j] = tmp;
     }
+    void test1()
+    {
+        std::cout<<"====================test1()"<<std::endl;
+        MyClass* c = new MyClass();
+        const int size = 4;
+        
+        char array[size] = {'e', 'a', 'b', 'c'};
+        char* buf = (char*)malloc(sizeof(char)*size);
+
+        int n = size-1;
+        int r = 3;
+        int k = 3;
+        c->nchoosek(array, buf, n, r, k);
+        free(buf);
+    } 
+
+    void test2()
+    {
+        std::cout<<"====================test2()"<<std::endl;
+        MyClass* c = new MyClass();
+        const int size = 3;
+        
+        char array[size] = {'e', 'a', 'b'};
+        char* buf = (char*)malloc(sizeof(char)*size);
+
+        int n = size-1;
+        int r = 1;
+        int k = 1;
+        c->nchoosek(array, buf, n, r, k);
+        free(buf);
+    } 
+
+    void test3()
+    {
+        std::cout<<"====================test3()"<<std::endl;
+        MyClass* c = new MyClass();
+        const int size = 2;
+        
+        char array[size] = {'e', 'a'};
+        char* buf = (char*)malloc(sizeof(char)*size);
+
+        int n = size-1;
+        int r = 1;
+        int k = 1;
+        c->nchoosek(array, buf, n, r, k);
+        free(buf);
+    } 
 };
 
 int main()
 {
     MyClass* c = new MyClass();
-    const int size = 3;
+    const int size = 4;
     int k = 0;
     int* arrayPt = (int*)malloc(sizeof(int)*size); 
-    c->permutate(arrayPt, size, k);
+    //c->permutate(arrayPt, size, k);
 
     std::cout<<"===================="<<std::endl;
     
-    char array[size] = {'a', 'b', 'c'};
+    char array[size] = {'e', 'a', 'b', 'c'};
     char* buf = (char*)malloc(sizeof(char)*size);
-    c->combination(array, buf, size, k);
-    free(buf);
+    //c->combination(array, buf, size, k);
+
+    
+    c->test1();
+    c->test2();
+    c->test3();
+
 }
