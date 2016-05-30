e060f468928534298fc4a9bc34b45f52209af56e
bsdshell <monosurface@gmail.com>
2016-05-29 23:09:15 -0700

    allocate

---
+++ Allocate.cpp
@@ -0,0 +1,24 @@
+#include<iostream>
+
+int* allocate(int size){
+    return new int[size];
+}
+
+void deallocate(int *pt){
+    delete pt;
+    pt = NULL;
+}
+
+int main() {
+    int size = 10;
+    int* pt = allocate(size);
+    for(int i=0; i<size; i++){
+        pt[i] = i; 
+    }
+
+    for(int i=0; i<size; i++){
+        printf("[%d]\n", pt[i]); 
+    }
+    deallocate(pt);
+}
+
--- hello.cpp
+++ hello.cpp
@@ -1,6 +1,5 @@
 #include<iostream>
 
-
 struct ListNode {
     struct ListNode	*pLast,
                     *pNext;
