895178828c2230e4dd35255ba15c8049573fb6c2
bsdshell <monosurface@gmail.com>
2016-06-02 19:04:00 -0700

    overlap

--- OverlapInterval.cpp
+++ OverlapInterval.cpp
@@ -3,22 +3,50 @@
 
 using namespace std;
 
-void sort(std::array arr){
-    int len = 10; //sizeof(arr)/sizeof(int);
-    for(int i=0; i<len; i++){
-        std::cout<<"["<<arr[i]<<"]"<<'\n';
-    } 
-}
+//[file=overlap.html title=""
+int getMaxCount(vector<int> arr, vector<int> dep){
+    const int len = arr.size(); 
+    int i=0, j=0;
+    int count = 0;
+    int max = 0;
+    vector<int> marr;
 
-int getMax(std::array arr, std::array dep){
-    int len = 0;
-    return len;
+    while(i < len || j < len){
+        if(i >= len){
+            marr.push_back(dep[j]);
+            count--;
+            j++;
+        }
+        else if(j >= len){
+            marr.push_back(arr[i]);
+            count++;
+            i++;
+        }
+        else {
+            if(arr[i] <= dep[j]){
+                marr.push_back(arr[i]);
+                count++;
+                i++;
+            }else{
+                marr.push_back(dep[j]);
+                count--;
+                j++;
+            }
+        }
+
+        if(count > max)
+            max = count;
+    }
+    return max;
 }
+//]
 
 int main(){
-    printf("bug\n"); 
-    int arr[] = {2, 1, 3};
-    sort(arr);
+    const int len = 5;
+    vector<int> arr = {1, 2, 5, 5, 9};
+    vector<int> dep = {4, 5, 9, 12, 12};
+    int max = getCount(arr, dep);
+    std::cout<<"max=["<<max<<"]"<<'\n';
 }
 
 
