234b59114172b7bcdacd94a0bdb7f11b2f7b5862
bsdshell <monosurface@gmail.com>
2016-06-04 14:28:53 -0700

    update

--- Tries/cpptries/main.cpp
+++ Tries/cpptries/main.cpp
@@ -26,14 +26,13 @@ public:
 
 void insert(Node* root, std::string str, int i){
     if(i < str.size()){
-        Node* node = root->map[str[i]];
         if(i == str.size() - 1){
-            if(node != NULL)
+            if(root->map[str[i]] != NULL)
                 root->map[str[i]]->terminal = true;
             else
                 root->map[str[i]] = new Node(true);
         }else{
-            if(node == NULL)
+            if(root->map[str[i]] == NULL)
                 root->map[str[i]] = new Node(false);
             
             insert(root->map[str[i]], str, i+1);
@@ -46,14 +45,13 @@ bool contain(Node* root, std::string str, int i){
         if(str.size() == 0)
             return root->terminal;
         
-        Node* node = root->map[str[i]];
         if(i == str.size() - 1){
-            if(node != NULL)
-                return node->terminal;
+            if(root->map[str[i]] != NULL)
+                return root->map[str[i]]->terminal;
             else
                 return false;
         }else{
-            return contain(node, str, i+1);
+            return contain(root->map[str[i]], str, i+1);
         }
     }else{
        return false;
@@ -74,11 +72,18 @@ void test0(){
     insert(root, "technologies", 0);
     insert(root, "technical", 0);
     insert(root, "techie", 0);
+    insert(root, "tear", 0);
+    insert(root, "ten", 0);
+    insert(root, "tencent", 0);
     
     std::cout<<contain(t->root, "te", 0)<<std::endl;
     std::cout<<contain(t->root, "tech", 0)<<std::endl;
     std::cout<<contain(t->root, "", 0)<<std::endl;
     std::cout<<contain(t->root, "techie", 0)<<std::endl;
+    std::cout<<contain(t->root, "techi", 0)<<std::endl;
+    std::cout<<contain(t->root, "t", 0)<<std::endl;
+    std::cout<<contain(t->root, "ten", 0)<<std::endl;
+    std::cout<<contain(t->root, "tencen", 0)<<std::endl;
 }
 
 int main(){
