76d687861fbc6d38ec7fb4c79347a746199d1a12
bsdshell <monosurface@gmail.com>
2016-06-05 00:41:03 -0700

    decimal

---
+++ DecimalBinary.cpp
@@ -0,0 +1,67 @@
+#include<iostream>
+#include<vector>
+#include "Aron.h"
+
+using namespace std;
+
+//[file=decimal-to-binary.html title=""
+vector<int> decimalBinary(int n){
+    vector<int> vec;
+    if(n == 0)
+        vec.push_back(0);
+    else if(n > 0){
+        while(n){
+            int r = n % 2;
+            n = n/2;
+            vec.push_back(r);
+        }
+    }
+    return vec;
+}
+//]
+
+
+void test0(){
+    printf("---------------------------------\n");
+    vector<int> v = decimalBinary(0);
+    printVector(v);
+    printf("---------------------------------\n");
+}
+void test1(){
+    printf("---------------------------------\n");
+    vector<int> v = decimalBinary(1);
+    printVector(v);
+    printf("---------------------------------\n");
+}
+void test2(){
+    printf("---------------------------------\n");
+    vector<int> v = decimalBinary(4);
+    printVector(v);
+    printf("---------------------------------\n");
+} 
+
+void test3(){
+    printf("---------------------------------\n");
+    vector<int> v = decimalBinary(10);
+    printVector(v);
+    printf("---------------------------------\n");
+} 
+
+void test4(){
+    printf("---------------------------------\n");
+    vector<int> v;
+    v.push_back(0);
+    v.push_back(1);
+    v.push_back(2);
+    printVector(v);
+    printf("---------------------------------\n");
+} 
+
+
+int main(){
+    test0();
+    test1();
+    test2();
+    test3();
+}
+
--- makefile
+++ makefile
@@ -1,3 +1,4 @@
 all: 
 	clang++ -g -v -std=c++11 -stdlib=libc++ -o  MergeSortedList MergeSortedList.cpp Aron.cpp
 	clang++ -g -v -std=c++11 -stdlib=libc++ -o  DataStructure  DataStructure.cpp 
+	clang++ -g -v -std=c++11 -stdlib=libc++ -o  DecimalBinary DecimalBinary.cpp  Aron.cpp
